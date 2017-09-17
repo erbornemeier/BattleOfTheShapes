b1faa24deb628e19c7184cf962d43b8f18080ddb
erbornemeier <erbornemeier@mines.edu>
2017-09-17 17:45:50 -0600

    Header files updated

---
+++ src/GameObjects/Bullet.h
@@ -0,0 +1,67 @@
+/*
+ *  CSCI 441 Computer Graphics, Fall 2017
+ *
+ *  Project: SQ1
+ *  File: Bullet.h
+ *
+ *  Author: Easton Bornemeier
+ *
+ *  Description:
+ *		This is the bullet class used for handling the movement and the collision
+ *      detection of the bullets and the enemies
+ */
+
+#pragma once
+#include <GLFW/glfw3.h>		// include GLFW framework header
+
+#ifdef __APPLE__			// if compiling on Mac OS
+	#include <OpenGL/gl.h>
+#else						// else compiling on Linux OS
+	#include <GL/gl.h>
+#endif
+
+// include GLM libraries and matrix functions
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+//std libs
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+#include "../Helpers/DrawingHelpers.h"
+#include "../Helpers/Point.h"
+
+class Bullet{
+
+public:
+
+	//constructors
+	Bullet();
+	Bullet(GLFWwindow* window, Point pos, float dir, int* colorPrim, int* colorSec);
+
+	//destructor
+	~Bullet();
+
+	//update and state setting
+	void update(const double& frameDiff);
+
+	//drawing
+	void drawBullet();
+
+	//bounds check
+	bool inBounds(int x, int y);
+
+private:
+
+	GLFWwindow* window;
+
+	//attributes
+	Point pos;
+	float dir;
+	float speed = 10;
+	int* colorPrim;
+	int* colorSec;
+
+};
---
+++ src/GameObjects/Player.h
@@ -0,0 +1,88 @@
+/*
+ *  CSCI 441 Computer Graphics, Fall 2017
+ *
+ *  Project: SQ1
+ *  File: Player.h
+ *
+ *  Author: Easton Bornemeier
+ *
+ *  Description:
+ *		This is the player class that handles drawing, shooting, and moving functionality
+ *		for the player
+ */
+
+#pragma once
+#include <GLFW/glfw3.h>		// include GLFW framework header
+
+#ifdef __APPLE__			// if compiling on Mac OS
+	#include <OpenGL/gl.h>
+#else						// else compiling on Linux OS
+	#include <GL/gl.h>
+#endif
+
+// include GLM libraries and matrix functions
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+//std libs
+#include <iostream>
+#include <set>
+#include <cmath>
+using namespace std;
+
+//custom game libs
+#include "Bullet.h"
+#include "../Helpers/DrawingHelpers.h"
+#include "../Helpers/Point.h"
+
+class Player{
+
+public:
+
+	//constructors
+	Player();
+	Player(GLFWwindow* window, Point pos, int* colorPrim, int* colorSec);
+
+	//getters
+	Point getPos();
+
+	//update and state setting
+	void update(const double& frameDiff);
+	void setState(bool up, bool down, bool left, bool right);
+
+	//actions
+	void shoot(float mx, float my);
+
+	//drawing
+	void draw();
+	void drawBullets();
+
+	//bounds check
+	void checkBullets();
+
+
+private:
+
+	//draw reference
+	GLFWwindow* window;
+
+	//attributes
+	Point pos;
+	float rot;
+	float transSpeed = 5;
+	float rotSpeed = 2;  
+	uint maxBullets = 50;
+	int* colorPrim;
+	int* colorSec;
+
+	//states
+	enum Translation {backwards = -1, stopped = 0, forwards = 1};
+	enum Rotation 	{leftRot = 1, noRot = 0, rightRot = -1};
+	int moving;
+	int rotating;
+	float timeToFire = 0;
+
+	//possessions
+	set<Bullet*> bullets;
+
+};
